6cbbf32de4540982d76e27dff04ae73375eb5da3
benoitc <benoitc@gmail.com>
2011-04-09 16:18:42 +0200

    we are not compatible any more with py25.

--- setup.py
+++ setup.py
@@ -19,8 +19,8 @@ import sys
 
 from couchapp import __version__
 
-if not hasattr(sys, 'version_info') or sys.version_info < (2, 5, 0, 'final'):
-    raise SystemExit("Couchapp requires Python 2.5 or later.")
+if not hasattr(sys, 'version_info') or sys.version_info < (2, 6, 0, 'final'):
+    raise SystemExit("Couchapp requires Python 2.6 or later.")
 
 executables = []
 setup_requires = []
@@ -59,7 +59,6 @@ def all_packages():
         'couchapp.restkit.manager',
         'couchapp.restkit.contrib',
         'couchapp.simplejson',
-        'couchapp.ssl',
         'couchapp.vendors',
         'couchapp.vendors.backends',
     ]
@@ -166,74 +165,6 @@ def find_library_file(compiler, libname, std_dirs, paths):
     else:
         assert False, "Internal error: Path not found in std_dirs or paths"
 
-
-def find_ssl():
-
-    # Detect SSL support for the socket module (via _ssl)
-    from distutils.ccompiler import new_compiler
-
-    compiler = new_compiler()
-    inc_dirs = compiler.include_dirs + ['/usr/include']
-
-    search_for_ssl_incs_in = [
-                          '/usr/local/ssl/include',
-                          '/usr/contrib/ssl/include/'
-                         ]
-    ssl_incs = find_file('openssl/ssl.h', inc_dirs,
-                         search_for_ssl_incs_in
-                         )
-    if ssl_incs is not None:
-        krb5_h = find_file('krb5.h', inc_dirs,
-                           ['/usr/kerberos/include'])
-        if krb5_h:
-            ssl_incs += krb5_h
-
-    ssl_libs = find_library_file(compiler, 'ssl',
-                                 ['/usr/lib'],
-                                 ['/usr/local/lib',
-                                  '/usr/local/ssl/lib',
-                                  '/usr/contrib/ssl/lib/'
-                                 ] )
-
-    if (ssl_incs is not None and ssl_libs is not None):
-        return ssl_incs, ssl_libs, ['ssl', 'crypto']
-
-    raise Exception("No SSL support found")
-
-
-socket_inc = "./couchapp/ssl/2.5.1"
-
-try:
-    import ssl
-except ImportError:
-    ssl = None
-
-if not ssl:
-    link_args = []
-    if sys.platform == 'win32':
-
-        # Assume the openssl libraries from GnuWin32 are installed in the
-        # following location:
-        gnuwin32_dir = os.environ.get("GNUWIN32_DIR", r"C:\Utils\GnuWin32")
-
-        # Set this to 1 for a dynamic build (depends on openssl DLLs)
-        # Dynamic build is about 26k, static is 670k
-        dynamic = int(os.environ.get("SSL_DYNAMIC", 0))
-
-        ssl_incs = [os.environ.get("C_INCLUDE_DIR") or os.path.join(gnuwin32_dir, "include")]
-        ssl_libs = [os.environ.get("C_LIB_DIR") or os.path.join(gnuwin32_dir, "lib")]
-        libs = ['ssl', 'crypto', 'wsock32']
-        if not dynamic:
-            libs = libs + ['gdi32', 'gw32c', 'ole32', 'uuid']
-            link_args = ['-static']
-    else:
-        ssl_incs, ssl_libs, libs = find_ssl()
-
-    extra['ext_modules']=[Extension('couchapp.ssl._ssl2', ['couchapp/ssl/_ssl2.c'],
-                                 include_dirs = ssl_incs + [socket_inc],
-                                 library_dirs = ssl_libs,
-                                 libraries = libs,
-                                 extra_link_args = link_args)]
 
 cmdclass = {'install_data': install_package_data }
  
