f482f0c20e871a2b7db9e6e10963b558060ea7d2
benoitc <bchesneau@gmail.com>
2011-05-08 06:50:11 +0200

    fix script

--- Couchapp.py
+++ Couchapp.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
 # This file is part of couchapp released under the Apache 2 license. 
 # See the NOTICE for more information.
 
-if __name__ == "__main__":
-    from couchapp import dispatch
+from couchapp.dispatch import run
+
+if __name__ == '__main__':
+    run()    
